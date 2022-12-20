a7e0f09d90dce0291a632bde1017c9dbbe8f24bd
Renan1fps <renan.sergipe68@gmail.com>
2022-12-20 16:02:59 -0300

    feat: add annotations to spring

--- src/main/java/anime/api/domain/Anime.java
+++ src/main/java/anime/api/domain/Anime.java
@@ -2,12 +2,27 @@ package anime.api.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
-
+@NoArgsConstructor
+@Entity
 public class Anime {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
     private String id;
     private String name;
 
