412b28626490c63a9c0ad0895db72d2965062335
Renan1fps <renan.sergipe68@gmail.com>
2022-12-20 11:28:14 -0300

    feat: improvements return controller

--- src/main/java/anime/api/controllers/AnimeController.java
+++ src/main/java/anime/api/controllers/AnimeController.java
@@ -3,6 +3,9 @@ package anime.api.controllers;
 import anime.api.domain.Anime;
 import anime.api.useCases.anime.ListAnimeUseCase;
 import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,7 @@ public class AnimeController {
     private final ListAnimeUseCase listAnimeUseCase;
 
     @GetMapping
-    public List<Anime> listAnime(){
-        return this.listAnimeUseCase.execute();
+    public ResponseEntity<List<Anime>> listAnime(){
+        return new ResponseEntity<>(this.listAnimeUseCase.execute(), HttpStatus.OK);
     }
 }
