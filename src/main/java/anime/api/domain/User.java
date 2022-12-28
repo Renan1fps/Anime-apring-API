c4d039322347d973f2f3bd44e91a7344bed57704
Renan1fps <renan.sergipe68@gmail.com>
2022-12-27 22:08:14 -0300

    feat: create entity for user

---
+++ src/main/java/anime/api/domain/User.java
@@ -0,0 +1,31 @@
+package anime.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+    private String email;
+    private String password;
+}
