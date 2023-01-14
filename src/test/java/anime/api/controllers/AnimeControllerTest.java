8825caa94d0694e76b9c9d11594a0d4d4c68febb
Renan1fps <renan.sergipe68@gmail.com>
2023-01-14 17:26:14 -0300

    test: Should return a anime anime when calls findAnimeByIdUseCase

--- src/test/java/anime/api/controllers/AnimeControllerTest.java
+++ src/test/java/anime/api/controllers/AnimeControllerTest.java
@@ -2,12 +2,14 @@ package anime.api.controllers;
 
 import anime.api.domain.Anime;
 import anime.api.mocks.AnimeMocks;
+import anime.api.useCases.anime.FindAnimeByIdUseCase;
 import anime.api.useCases.anime.ListAnimeUseCase;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -24,10 +26,16 @@ class AnimeControllerTest {
     @Mock // Classes que quero mockar
     ListAnimeUseCase listAnimeUseCaseMock;
 
+    @Mock
+    FindAnimeByIdUseCase findAnimeByIdUseCaseMock;
+
     @BeforeEach
     void setup(){
         List<Anime> animeList = new ArrayList<>(List.of(AnimeMocks.animeSaved()));
+        Anime anime = AnimeMocks.animeSaved();
+
         BDDMockito.when(listAnimeUseCaseMock.execute()).thenReturn(animeList);
+        BDDMockito.when(findAnimeByIdUseCaseMock.execute(ArgumentMatchers.anyString())).thenReturn(anime);
     }
 
     @Test
@@ -40,5 +48,18 @@ class AnimeControllerTest {
         Assertions.assertThat(animeList).isNotNull().hasSize(1);
         Assertions.assertThat(animeList.get(0).getName()).isEqualTo(expectedName);
         Assertions.assertThat(animeList.get(0).getName()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Should return a anime anime when calls findAnimeByIdUseCase")
+    void Should_ReturnAnime_WhenCallsFindById() {
+        String expectedId = AnimeMocks.animeSaved().getId();
+        Anime expectAnime = AnimeMocks.animeSaved();
+
+        Anime anime = animeController.getById(expectedId).getBody();
+
+        Assertions.assertThat(anime).isNotNull();
+        Assertions.assertThat(anime).isEqualTo(expectAnime);
+        Assertions.assertThat(anime).isInstanceOf(Anime.class);
     }
 }
